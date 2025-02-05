2c7e6460733e1aee91c7c8def70ed0af68c3171b
JEJO J <147123805+jejo205713@users.noreply.github.com>
2025-02-05 11:44:05 +0530

    Add files via upload

---
+++ mysql_kali_install.sh
@@ -0,0 +1,19 @@
+echo "installation script by jejo to install MYSQL-SERVER on kali linux !"
+
+echo "🔍 Updating package lists..."
+sudo apt update -y
+
+echo "📦 Installing snapd..."
+sudo apt install snapd -y
+
+echo "✅ Enabling and starting snapd and apparmor..."
+sudo systemctl enable --now snapd apparmor
+
+echo "⏳ Installing MySQL Server (Snap package)..."
+snap install --channel=8.0 mysql-server
+
+echo "🚀 Starting MySQL service..."
+sudo systemctl start mysql
+
+echo "✅ MySQL installation and setup completed successfully!"
+
